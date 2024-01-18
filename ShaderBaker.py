712d3e76c90f8cc8c48dbeb6222d07a7cb5994bd
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-18 08:07:47 -0800

    added panel to underneath baker

--- ShaderBaker.py
+++ ShaderBaker.py
@@ -1,4 +1,24 @@
-import bpy as blend
+import bpy
+
+class ShaderBakerPanel(bpy.types.Panel):
+    '''
+    User Interface for the ShaderBaker tool
+    '''
+    bl_label = "Shader Baker"
+    bl_idname = "PT_ShaderBakerPanel"
+    
+    # Defines where we place this panel
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "render"
+    
+    bl_options = {'DEFAULT_CLOSED'}
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.label(text="Shader Baker Panel")
+    
 
 class ShaderBaker:
     '''
@@ -8,14 +28,22 @@ class ShaderBaker:
     def __init__(self, scene):
         self.scene = scene
         
+def register():
+    bpy.utils.register_class(ShaderBakerPanel)
+    
+def unregister():
+    bpy.utils.unregister_class(ShaderBakerPanel)
+        
 def main():
     '''
     Main entry point to run code
     '''
     
-    shader_baker = ShaderBaker(blend.context.scene)
+    register()
     
-    print("Shader Baker Tool")
+#    shader_baker = ShaderBaker(blend.context.scene)
+    
+#    print("Shader Baker Tool")
     
 if __name__ == "__main__":
     main()
